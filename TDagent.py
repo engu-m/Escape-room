07678db7a246df1d536f04441d47a3103b8d367f
Enguerrand Monard <enguerrand.monard@gmail.com>
2022-02-24 17:47:03 +0100

    simple episode run (random policy)
    need to fix agent

--- TDagent.py
+++ TDagent.py
@@ -1,33 +1,35 @@
 """Agents for EscapeRoom"""
 import numpy as np
 
-class TDAgent():
 
+class TDAgent:
     def __init__(self, agent_info={}):
 
-        self.rand_generator = np.random.RandomState(agent_info.get("seed"))
+        # self.rand_generator = np.random.RandomState(agent_info.get("seed"))
 
-        # Policy will be given, recall that the goal is to accurately estimate its corresponding value function. 
+        # Policy will be given, recall that the goal is to accurately estimate its corresponding value function.
         self.policy = agent_info.get("policy")
         # Discount factor (gamma) to use in the updates.
         self.discount = agent_info.get("discount")
         # The learning rate or step size parameter (alpha) to use in updates.
         self.step_size = agent_info.get("step_size")
 
-        self.values = np.zeros((self.policy.shape[0],))
-          
+        self.values = np.zeros(self.policy.shape[:-1])  # exlude action dimension
+
     def agent_start(self, state):
 
-        action = self.rand_generator.choice(range(self.policy.shape[1]), p=self.policy[state])
+        action = self.rand_generator.choice(range(self.policy.shape[-1]), p=self.policy[state])
         self.last_state = state
         return action
-            
+
     def agent_step(self, reward, state):
 
         target = reward + self.discount * self.values[state]
-        self.values[self.last_state] = self.values[self.last_state] + self.step_size * (target - self.values[self.last_state])
+        self.values[self.last_state] = self.values[self.last_state] + self.step_size * (
+            target - self.values[self.last_state]
+        )
 
-        action = self.rand_generator.choice(range(self.policy.shape[1]), p=self.policy[state])
+        action = self.rand_generator.choice(range(self.policy.shape[-1]), p=self.policy[state])
         self.last_state = state
 
         return action
@@ -35,4 +37,6 @@ class TDAgent():
     def agent_end(self, reward):
 
         target = reward
-        self.values[self.last_state] = self.values[self.last_state] + self.step_size * (target - self.values[self.last_state])
+        self.values[self.last_state] = self.values[self.last_state] + self.step_size * (
+            target - self.values[self.last_state]
+        )
