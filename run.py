af934847443c79619c4c0f74e9d24e9c1890402b
Enguerrand Monard <enguerrand.monard@gmail.com>
2022-03-02 15:16:17 +0100

    move function def to top + rename file

--- run.py
+++ run.py
@@ -11,40 +11,6 @@ from Environment import EscapeRoomEnvironment
 from agent import QLearningAgent, ExpectedSarsa
 import viz
 
-env_params = {
-    "grid_width": 4,
-    "grid_height": 5,
-}
-
-fps = 0.2
-
-
-agent_info = {
-    "num_actions": 4,
-    "epsilon": 0.1,
-    "tuple_state": (env_params["grid_height"], env_params["grid_width"]),
-    "discount": 1,
-    "step_size": 0.8,
-    "seed": 3,
-}
-
-agents = {
-    "ExpectedSarsa": ExpectedSarsa(agent_init_info=agent_info),
-    "QLearningAgent": QLearningAgent(agent_init_info=agent_info),
-}
-
-num_runs = 5000
-runs_nb_to_show = range(10)  # show 10 first runs
-runs_nb_to_show = range(num_runs - 10, num_runs)  # show 10 last runs
-runs_nb_to_show = [
-    min(k * num_runs // 10, num_runs - 1) for k in range(10 + 1)
-]  # show all k*10% runs
-runs_nb_to_show = [0, num_runs - 1]  # show first and last runs only
-runs_nb_to_show = []  # show no run on terminal
-
-n_first_run_visit = 120  # number of first run visits to show
-n_last_run_visit = 300  # number of last run visits to show
-
 
 def run(agent_name, agent, env, **run_parameters):
     num_runs = run_parameters["num_runs"]
@@ -87,30 +53,64 @@ def run(agent_name, agent, env, **run_parameters):
 
     if viz_results:
         save_dir = Path("Escape-Room-RL/viz") / agent_name
-        save_dir.mkdir(exist_ok=True)
+        save_dir.mkdir(exist_ok=True, parents=True)
         viz.plot_best_action_per_state(agent.q, num_runs, save_directory=save_dir, **viz_params)
         viz.plot_n_first_visits(first_state_visits, num_runs, save_directory=save_dir, **viz_params)
         viz.plot_n_last_visits(last_state_visits, num_runs, save_directory=save_dir, **viz_params)
         viz.plot_q_value_estimation(agent.q, num_runs, save_directory=save_dir, **viz_params)
 
 
-if __name__ == "__main__":
-    run_parameters = {
-        "num_runs": num_runs,
-        "runs_nb_to_show": runs_nb_to_show,
-        "fps": fps,
-        "n_first_run_visit": n_first_run_visit,
-        "n_last_run_visit": n_last_run_visit,
-        "viz_results": True,
-        "viz_params": {
-            "save": True,
-            "show": False,
-            "cmap": "magma",
-            "max_fontsize": 40,
-            "block_show": True,
-        },
-    }
+env_params = {
+    "grid_width": 4,
+    "grid_height": 5,
+}
 
-    for agent_name, agent in agents.items():
-        env = EscapeRoomEnvironment(env_info=env_params)
-        run(agent_name, agent, env, **run_parameters)
+fps = 0.2
+
+
+agent_info = {
+    "num_actions": 4,
+    "epsilon": 0.1,
+    "tuple_state": (env_params["grid_height"], env_params["grid_width"]),
+    "discount": 1,
+    "step_size": 0.8,
+    "seed": 3,
+}
+
+agents = {
+    "ExpectedSarsa": ExpectedSarsa(agent_init_info=agent_info),
+    "QLearningAgent": QLearningAgent(agent_init_info=agent_info),
+}
+
+num_runs = 5000
+runs_nb_to_show = range(10)  # show 10 first runs
+runs_nb_to_show = range(num_runs - 10, num_runs)  # show 10 last runs
+runs_nb_to_show = [
+    min(k * num_runs // 10, num_runs - 1) for k in range(10 + 1)
+]  # show all k*10% runs
+runs_nb_to_show = [0, num_runs - 1]  # show first and last runs only
+runs_nb_to_show = []  # show no run on terminal
+
+n_first_run_visit = 120  # number of first run visits to show
+n_last_run_visit = 300  # number of last run visits to show
+
+
+run_parameters = {
+    "num_runs": num_runs,
+    "runs_nb_to_show": runs_nb_to_show,
+    "fps": fps,
+    "n_first_run_visit": n_first_run_visit,
+    "n_last_run_visit": n_last_run_visit,
+    "viz_results": True,
+    "viz_params": {
+        "save": True,
+        "show": False,
+        "cmap": "magma",
+        "max_fontsize": 40,
+        "block_show": True,
+    },
+}
+
+for agent_name, agent in agents.items():
+    env = EscapeRoomEnvironment(env_info=env_params)
+    run(agent_name, agent, env, **run_parameters)
